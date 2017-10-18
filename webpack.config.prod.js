e1964abf8a77cf4a4a940ffbbd1f30c266c1d0c2
Daniega <daniel.gaishuber@gmail.com>
2017-10-18 15:16:35 +0300

    server config

---
+++ webpack.config.prod.js
@@ -0,0 +1,22 @@
+var config = require('./webpack.config.js');
+var webpack = require('webpack');
+
+config.plugins.push(
+    new webpack.DefinePlugin({
+        "process.env": {
+            "NODE_ENV": JSON.stringify("production"),
+            'API_HOST': JSON.stringify('https://tic-tac-toe-illya.herokuapp.com/')
+
+        }
+    })
+);
+
+config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    })
+);
+
+module.exports = config;
