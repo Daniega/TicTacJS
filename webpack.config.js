bfe6f3789306cfa598b763a5a180380e738a7d70
Daniega <daniel.gaishuber@gmail.com>
2017-10-19 21:15:48 +0300

    change host to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports={
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('development'),
-                'API_HOST': JSON.stringify('http://localhost:3000')
+                'API_HOST': JSON.stringify('https://tictacdani.herokuapp.com/')
             }
 
         })
