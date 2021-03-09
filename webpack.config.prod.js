d66bc955f6d0e0ce91f1b740bf0123ac1a04dc8c
Daniega <daniel.gaishuber@gmail.com>
2021-03-09 12:42:02 +0200

    updated some refs in webpack config file

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,21 +2,20 @@ var config = require('./webpack.config.js');
 var webpack = require('webpack');
 
 config.plugins.push(
-    new webpack.DefinePlugin({
-        "process.env": {
-            "NODE_ENV": JSON.stringify("production"),
-            'API_HOST': JSON.stringify('http://localhost:3000')
-
-        }
-    })
+	new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: JSON.stringify('production'),
+			API_HOST: JSON.stringify('https://tic-tac-chat.herokuapp.com/'),
+		},
+	})
 );
 
 config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        }
-    })
+	new webpack.optimize.UglifyJsPlugin({
+		compress: {
+			warnings: false,
+		},
+	})
 );
 
 module.exports = config;
